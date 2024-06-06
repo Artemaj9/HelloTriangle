a29e0dbad979673a8e72d383a168161e11c0810f
ArtemMoiseev <moiseev@physics.msu.ru>
2024-06-06 22:04:48 +0300

    Initial Commit

---
+++ HelloTriangle/HelloTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTriangleApp.swift
+//  HelloTriangle
+//
+//  Created by Artem on 06.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
